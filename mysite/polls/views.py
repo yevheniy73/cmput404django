e9671f489e47e80c69f6489ced34d1819b2a4b94
yevheniy73 <kaznovsk@ualberta.ca>
2023-10-04 15:15:03 -0600

    serializer and lab finished

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import QuestionSerializer
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -5,6 +8,26 @@ from django.views import generic
 
 from .models import Choice, Question
 
+@api_view(['GET'])
+def get_questions(request):
+    """
+    Get the list of questions on our website
+    """
+    questions = Question.objects.all()
+    serializer = QuestionSerializer(questions, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def update_question(request, pk):
+    """
+    Get the list of questions on our website
+    """
+    questions = Question.objects.get(id=pk)
+    serializer = QuestionSerializer(questions, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.update(questions, serializer.validated_data)
+        return Response(serializer.data)
+    return Response(status=400, data=serializer.errors)
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
